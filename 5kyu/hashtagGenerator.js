8b0c6ea675db23f0256d37e387ec55179fdb84fc
Victor Fernandez <vmfs99@gmail.com>
2020-02-18 15:17:34 -0500

    finished kata

---
+++ 5kyu/hashtagGenerator.js
@@ -0,0 +1,8 @@
+function generateHashtag (str) {
+
+    var hashtag = str.split(' ').reduce(function(tag, word) {
+      return tag + word.charAt(0).toUpperCase() + word.substring(1);
+    }, '#');
+    
+    return hashtag.length == 1 || hashtag.length > 140 ? false : hashtag;
+  }
